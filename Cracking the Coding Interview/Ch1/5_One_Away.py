7d8e37e7f718044dbcc92663c1a2f2ecba00097a
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-15 23:50:26 -0500

    Implement One Away with print statements to check for errors.

--- Cracking the Coding Interview/Ch1/5_One_Away.py
+++ Cracking the Coding Interview/Ch1/5_One_Away.py
@@ -10,6 +10,33 @@
 import unittest
 #-------------------------------------------------------------
 def one_away(s1,s2):
-    pass
-
+    if abs(len(s1) - len (s2)) > 1:
+        return False
+    
+    count = 0
+    s1Dict = {}
+    for char in s1:
+        s1Dict[char] = s1Dict.get(char,0) + 1
+    
+    for char in s2:
+        print('now checking', char)
+        if char not in s1:
+            print('not in',s1)
+            count += 1
+            if count > 1:
+                print('more than one difference', s1,s2)
+                return False
+        elif s1Dict[char] == 0:
+            print('no more', char, 'left')
+            return False
+        else:
+            s1Dict[char] -= 1
+            print('\'', char, '\' removed, ', s1Dict[char], 'left')
+    return True
 #-------------------------------------------------------------
+
+print(one_away('pale','ple'))
+print(one_away('pales','pale'))
+print(one_away('pale','bale'))
+print(one_away('pale','bake'))
+print(one_away('pale','lle'))
