b59faf607eb648e1f35f20197181f2fa2e86372b
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-18 23:46:48 -0500

    Add String Compression File.

---
+++ Cracking the Coding Interview/Ch1/6_String_Compression.py
@@ -0,0 +1,23 @@
+# String Compression: Implement a method to perform basic string compression using the counts
+# of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+# "compressed" string would not become smaller than the original string, your method should return
+# the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+
+import unittest
+#-------------------------------------------------------------
+
+def compress_string(s):
+    compressed_str = ''
+
+    return compressed_str
+
+#-------------------------------------------------------------
+# Time: 
+# Space: 
+#-------------------------------------------------------------
+
+# class Test(unittest.TestCase):
+
+
+# if __name__ == '__main__':
+#     unittest.main()
