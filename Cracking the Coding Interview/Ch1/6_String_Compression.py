c2b118e4fed761a94e1ec51a3057fe13a10ea632
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-19 19:12:31 -0500

    Implement String Compression and add edge cases.

--- Cracking the Coding Interview/Ch1/6_String_Compression.py
+++ Cracking the Coding Interview/Ch1/6_String_Compression.py
@@ -7,17 +7,45 @@ import unittest
 #-------------------------------------------------------------
 
 def compress_string(s):
+    if len(s) <= 2:
+        return s
+    
     compressed_str = ''
+    i = 1
+    curr_char = s[0]
+    count = 1
 
-    return compressed_str
+    while i < len(s):
+        if curr_char != s[i]:
+            compressed_str = compressed_str + curr_char + str(count)
+            curr_char = s[i]
+            count = 1
+            if i == len(s) - 1:
+                compressed_str = compressed_str + curr_char + str(1)
+        else:
+            count += 1
+            if i == len(s) - 1:
+                compressed_str = compressed_str + curr_char + str(count)
+
+        i += 1
+    return compressed_str if len(compressed_str) < len(s) else s
+
 
 #-------------------------------------------------------------
 # Time: 
 # Space: 
 #-------------------------------------------------------------
 
-# class Test(unittest.TestCase):
+class Test(unittest.TestCase):
+    def test_one_char(self):
+        answer = compress_string('a')
+        expected = 'a'
+        self.assertEqual(answer, expected)
+    
+    def test_two_char(self):
+        answer = compress_string('aa')
+        expected = 'aa'
+        self.assertEqual(answer, expected)
 
-
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
