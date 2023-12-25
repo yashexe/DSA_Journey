b2b37f87871226f5780d207b92dc7c742ce3a754
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-25 18:18:01 -0500

    Update Zero_Matrix

--- CTCI/Ch1/8_Zero_Matrix.py
+++ CTCI/Ch1/8_Zero_Matrix.py
@@ -5,18 +5,25 @@ import unittest
 #-------------------------------------------------------------
 
 def zero_matrix(matrix):
-    zero_dict = {}
+    zeros = []
 
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] == 0:
-                zero_dict[f'{i},{j}'] = 1
-    
-    
+                zeros.append(f'{i}{j}')
+
+    print(zeros)
+
+    rows = set(s[0] for s in zeros)
+    columns = set(s[1] for s in zeros)
+
+    print(rows)
+    print(columns)
     return matrix
 
+zero_matrix([[1,0,3],[4,0,0],[0,8,9]])
 #-------------------------------------------------------------
-# Time: 
+# Time:
 # Space: 
 #-------------------------------------------------------------
 
