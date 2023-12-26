65c3083de220d9dec94efea4b89c1b9aa6340d7b
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-26 18:10:55 -0500

    Add String Rotation.

---
+++ CTCI/Ch1/9_String_Rotation.py
@@ -0,0 +1,20 @@
+# String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings s1 and s2, write code to check if s2 is a rotation of s1, using only one call
+# to isSubstring. e.g. 'waterbottle' is a rotation of 'elttobretaw'
+
+import unittest
+#-------------------------------------------------------------
+
+def String_Rotate(s1,s2):
+
+    return False
+
+#-------------------------------------------------------------
+# Time: 
+# Space: 
+#-------------------------------------------------------------
+
+# class test(unittest.TestCase):
+
+        
+# if __name__ == '__main__':
+#     unittest.main()
