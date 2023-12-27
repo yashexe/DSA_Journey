93792b2b07b2c868ac8f916aaaff8ef09ecce321
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-26 19:43:11 -0500

    Implement String Rotation.

--- CTCI/Ch1/9_String_Rotation.py
+++ CTCI/Ch1/9_String_Rotation.py
@@ -2,19 +2,35 @@
 # to isSubstring. e.g. 'waterbottle' is a rotation of 'elttobretaw'
 
 import unittest
+
 #-------------------------------------------------------------
+def isSubstring(s1,s2):
+    return s2 in s1
 
 def String_Rotate(s1,s2):
-
-    return False
+    if len(s1) != len(s2) or len(s1) == 0:
+        return False
+    
+    return isSubstring(s1 + s1, s2)
+    
 
 #-------------------------------------------------------------
-# Time: 
-# Space: 
+# Time: O(n) - 'in' makes isSubString O(mn), though this function isnt tested in this Q
+# Space: O(n)
 #-------------------------------------------------------------
 
-# class test(unittest.TestCase):
+class test(unittest.TestCase):
+    def test_single_char(self):
+        self.assertTrue(String_Rotate('s','s'))
 
+    def test_diff_length(self):
+        self.assertFalse(String_Rotate('shares','share'))
+    
+    def test_single_str(self):
+        self.assertFalse(String_Rotate('','s'))
+
+    def test_rotation(self):
+        self.assertTrue(String_Rotate('waterbottle','erbottlewat'))
         
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
