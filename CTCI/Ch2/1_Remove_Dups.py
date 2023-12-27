87dc15d12ef7b243ef34195df711c95720f43472
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-26 19:55:52 -0500

    Add Remove Dups.

---
+++ CTCI/Ch2/1_Remove_Dups.py
@@ -0,0 +1,21 @@
+# Remove Dups: Write code to remove duplicates from an unsorted linked list.
+
+# FOLLOW-UP: How would you solve this problem if a temporary buffer is not allowed?
+
+import unittest
+
+#-------------------------------------------------------------
+def Remove_Dups(head):
+    
+    return head
+
+
+#-------------------------------------------------------------
+# Time: 
+# Space: 
+#-------------------------------------------------------------
+
+# class test(unittest.TestCase):
+        
+# if __name__ == '__main__':
+#     unittest.main()
