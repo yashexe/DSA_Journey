bcede343268eb71cc153b079193d3f8280fa778f
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-30 18:40:45 -0500

    Add new function for Remove dups with a no-buffer implementation. re-implement original method with no dummy header.

--- CTCI/Ch2/1_Remove_Dups.py
+++ CTCI/Ch2/1_Remove_Dups.py
@@ -10,23 +10,47 @@ def Remove_Dups(sll):
     if sll.is_empty() or sll.head.next is None:
         return sll
 
-    values_set = set()
+    values_set = set([sll.head.value])
 
-    dummy = Node(0)
-    dummy.next = sll.head
+    curr = sll.head
 
-    while dummy.next:
+    while curr.next:
         
-        if dummy.next.value not in values_set:
-            values_set.add(dummy.next.value)
-            dummy = dummy.next
+        if curr.next.value not in values_set:
+            values_set.add(curr.next.value)
+            curr = curr.next
         else:
-            dummy.next = dummy.next.next
+            curr.next = curr.next.next
 
     return sll 
 
 #-------------------------------------------------------------
 # Time: O(n) - Iterating through loop once
+# Space: O(k) - all unique Node Values
+#-------------------------------------------------------------
+# What if there is no buffer(eg.A set or dictionary)?
+#-------------------------------------------------------------
+
+def Remove_Dups_2(sll):
+    if sll.is_empty() or sll.head.next is None:
+        return sll
+
+    curr = sll.head
+
+    while curr:
+        runner = curr
+
+        while runner.next:
+            if curr.value == runner.next.value:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        curr = curr.next
+
+    return sll
+
+#-------------------------------------------------------------
+# Time: O(n^2) - Iterating through loop once for every value
 # Space: O(k) - all unique Node Values
 #-------------------------------------------------------------
 
@@ -66,7 +90,41 @@ class test(unittest.TestCase):
         test.group_add([1,2,3,4,5])
         Remove_Dups(test)
         self.assertEqual(test.to_list(),[1,2,3,4,5])
+    #No Buffer
+    def test_no_sll_2(self):
+        test = LinkedList()
+        Remove_Dups_2(test)
+        self.assertEqual(test.to_list(),[])
 
+    def test_singular_node_2(self):
+        test = LinkedList()
+        test.group_add([1])
+        Remove_Dups_2(test)
+        self.assertEqual(test.to_list(),[1])
+
+    def test_singular_dup_start_2(self):
+        test = LinkedList()
+        test.group_add([1,1,2])
+        Remove_Dups_2(test)
+        self.assertEqual(test.to_list(),[1,2])
+
+    def test_singular_dup_end_2(self):
+        test = LinkedList()
+        test.group_add([1,2,2])
+        Remove_Dups_2(test)
+        self.assertEqual(test.to_list(),[1,2])
+
+    def test_multiple_dup_2(self):
+        test = LinkedList()
+        test.group_add([1,1,2,3,3,4,5,5])
+        Remove_Dups_2(test)
+        self.assertEqual(test.to_list(),[1,2,3,4,5])
+
+    def test_no_dup_2(self):
+        test = LinkedList()
+        test.group_add([1,2,3,4,5])
+        Remove_Dups_2(test)
+        self.assertEqual(test.to_list(),[1,2,3,4,5])
 
 if __name__ == '__main__':
     unittest.main()
