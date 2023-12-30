297c45e0b1213de9f1f68a1f18559acaa597d0aa
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-30 02:20:44 -0500

    Add Return Kth to Last Question File.

---
+++ CTCI/Ch2/2_Return_Kth_to_Last.py
@@ -0,0 +1,23 @@
+# Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
+
+from LinkedList import LinkedList,Node
+import unittest
+
+#-------------------------------------------------------------
+
+def kth_to_last(sll,k):
+
+
+    return sll 
+
+#-------------------------------------------------------------
+# Time:
+# Space:
+#-------------------------------------------------------------
+
+# class test(unittest.TestCase):
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
