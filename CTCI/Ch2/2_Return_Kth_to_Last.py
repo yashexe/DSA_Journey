57c82c4b6c9573f7724a6ba0121aa0a66d1a26b4
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-12-30 19:10:54 -0500

    Implement kth_to_last function

--- CTCI/Ch2/2_Return_Kth_to_Last.py
+++ CTCI/Ch2/2_Return_Kth_to_Last.py
@@ -6,9 +6,27 @@ import unittest
 #-------------------------------------------------------------
 
 def kth_to_last(sll,k):
+    curr = sll.head
+    sll_length = 0
 
+    while curr:
+        sll_length += 1
+        curr = curr.next
 
-    return sll 
+    if k > sll_length:
+        return None
+
+    curr = sll.head
+
+    while sll_length != k and curr:
+        curr = curr.next
+        sll_length -= 1
+
+    return curr.value 
+
+sll = LinkedList()
+sll.group_add([1,2,3,4,5])
+print(kth_to_last(sll,3))
 
 #-------------------------------------------------------------
 # Time:
