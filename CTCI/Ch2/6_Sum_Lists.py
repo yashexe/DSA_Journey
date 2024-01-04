8e99f68a35f89b34f88a17a467fc79b0a361d815
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-03 22:41:42 -0500

    Add Sum_Lists.py.

---
+++ CTCI/Ch2/6_Sum_Lists.py
@@ -0,0 +1,27 @@
+# Sum Lists: You have two numbers represented by a linked list, where each node contains a single
+# digit. The digits are stored in reverse order, such that the 1's digit is at the head of the
+# Write a function that adds the two numbers and returns the sum as a linked list.
+# EXAMPLE:
+# INPUT: 7->1->6 + 5->9->2
+# Output: 2->1->9
+# FOLLOW UP: Suppose the digits are stored in forward order. Repeat the above problem.
+# INPUT: 6->1->7 + 2->9->5
+# Output: 9->1->2
+
+from LinkedList import LinkedList,Node
+import unittest
+#-------------------------------------------------------------
+
+def sum_sll(list1,list2):
+    sum = LinkedList()
+
+    return sum
+#-------------------------------------------------------------
+# Time: 
+# Space:
+#-------------------------------------------------------------
+
+# class test(unittest.TestCase):
+
+# if __name__ == '__main__':
+#     unittest.main()
