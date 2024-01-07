28afa96629081e2c698012ddcc8489fcd678b7b2
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-07 18:35:38 -0500

    Implement Sum Lists through iteration of list.

--- CTCI/Ch2/6_Sum_Lists.py
+++ CTCI/Ch2/6_Sum_Lists.py
@@ -10,30 +10,31 @@ import unittest
 #-------------------------------------------------------------
 
 def sum_sll(list1,list2):
-    sum = LinkedList()
+
+    addend1 = list1.head
+    addend2 = list2.head
     
-    num1 = to_int(list1)
-    num2 = to_int(list2)
+    while addend1:
+        addend2.value += addend1.value
 
-    sum.group_add([int(i) for i in reversed(str(num1 + num2))])
+        if addend2.value >= 10:
+            addend2.value -= 10
 
-    return sum
+            if addend2.next:
+                addend2.next.value += 1
+            else:
+                addend2.next = Node(1)
+        
+        if not addend2.next and addend1.next:
+            addend2.next = Node(0)
+        
+        addend1 = addend1.next
+        addend2 = addend2.next
 
-def to_int(list):
-    num = 0
-    i = 0
-    curr = list.head
-
-    while curr:
-        num += curr.value*pow(10,i)
-        i += 1
-        curr = curr.next
-
-    return num
-
+    return list2
 #-------------------------------------------------------------
 # Time:  O(max(m, n))
-# Space:  O(max(m, n))
+# Space:  O(1)
 #-------------------------------------------------------------
 # FOLLOW UP: Suppose the digits are stored in forward order. Repeat the above problem.
 # INPUT: 6->1->7 + 2->9->5
@@ -47,18 +48,36 @@ def sum_sll2(list1,list2):
         return list2
     elif list1.head.value != 0 and list2.head.value == 0:
         return list1
+
+    if list1.length() > list2.length():
+        sll1 = list1.head
+        sll2 = list2.head
+    else:
+        sll1 = list2.head
+        sll2 = list1.head
     
+    if list1.length() != list2.length():
+        zeros = LinkedList()
+
+        for i in range(abs(list1.length() - list2.length())):
+            zeros.add(0)
+        curr = zeros.head
+
+        while curr.next:
+            curr = curr.next
+        curr.next = sll2
+        sll2 = zeros.head
+    
+    result = 0
+    while sll1 and sll2:
+        result = result*10 + sll1.value + sll2.value
+        sll1 = sll1.next
+        sll2 = sll2.next
+
     sum = LinkedList()
-        
-    list1_arr = list1.to_list()
-    list2_arr = list2.to_list()
-
-    sum_str = str(int(''.join(map(str,list1_arr))) + int(''.join(map(str,list2_arr))))
-
-    sum.group_add([int(i) for i in sum_str])
-
+    sum.group_add([int(i) for i in str(result)])
+    
     return sum
-
 #-------------------------------------------------------------
 # Time:  O(max(m, n))
 # Space: O(m + n)
@@ -75,17 +94,57 @@ class test(unittest.TestCase):
 
         self.assertEqual(sum1,[0])
         self.assertEqual(sum2,[0])
+    def test_carry(self):
+        list1 = LinkedList()
+        list2 = LinkedList()
+        list1.group_add([4,2,7])
+        list2.group_add([9,4])
+
+        sum1 = sum_sll(list1,list2).to_list()
+
+        list3 = LinkedList()
+        list4 = LinkedList()
+        list3.group_add([7,2,4])
+        list4.group_add([4,9])
+
+        sum2 = sum_sll2(list3,list4).to_list()
+
+        self.assertEqual(sum1,[3,7,7])
+        self.assertEqual(sum2,[7,7,3])
+    def test_carry_flipped(self):
+        list1 = LinkedList()
+        list2 = LinkedList()
+        list1.group_add([9,4])
+        list2.group_add([4,2,7])
+
+        sum1 = sum_sll(list1,list2).to_list()
+
+        list3 = LinkedList()
+        list4 = LinkedList()
+        list3.group_add([4,9])
+        list4.group_add([7,2,4])
+
+        sum2 = sum_sll2(list3,list4).to_list()
+
+        self.assertEqual(sum1,[3,7,7])
+        self.assertEqual(sum2,[7,7,3])
     def test_incr_digits(self):
         list1 = LinkedList()
         list2 = LinkedList()
-        list1.add(1)
-        list2.add(9)
+        list1.group_add([9,9,9])
+        list2.group_add([1])
 
         sum1 = sum_sll(list1,list2).to_list()
-        sum2 = sum_sll2(list1,list2).to_list()
 
-        self.assertEqual(sum1,[0,1])
-        self.assertEqual(sum2,[1,0])
+        list3 = LinkedList()
+        list4 = LinkedList()
+        list3.group_add([9,9,9])
+        list4.group_add([1])
+
+        sum2 = sum_sll2(list3,list4).to_list()
+
+        self.assertEqual(sum1,[0,0,0,1])
+        self.assertEqual(sum2,[1,0,0,0])
 
 if __name__ == '__main__':
     unittest.main()
