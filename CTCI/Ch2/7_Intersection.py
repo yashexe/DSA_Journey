70cf32a717e3c4b81cacf77f32da5b77e3245af6
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-07 19:40:04 -0500

    Add intersection.py.

---
+++ CTCI/Ch2/7_Intersection.py
@@ -0,0 +1,22 @@
+# Intersection: Given two singly linked lists, determine if the two lists intersect. Return the
+# intersecting node. Note that the intersection is defined based on reference, not value. That is, if the
+# kth node of the first linked list is the exact same node(by reference) as the jth node of the second
+# linked list, then they are intersecting.
+
+from LinkedList import LinkedList,Node
+import unittest
+#-------------------------------------------------------------
+
+def is_intersection(sll1,sll2):
+    node = Node()
+
+    return node
+
+#-------------------------------------------------------------
+# Time:
+# Space:
+#-------------------------------------------------------------
+# class test(unittest.TestCase):
+
+# if __name__ == '__main__':
+#     unittest.main()
