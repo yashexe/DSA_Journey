a1f6cbf4926afbcc1e843debbf9e69ce4ace11af
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-07 21:41:39 -0500

    Implement intersection.py.

--- CTCI/Ch2/7_Intersection.py
+++ CTCI/Ch2/7_Intersection.py
@@ -8,15 +8,57 @@ import unittest
 #-------------------------------------------------------------
 
 def is_intersection(sll1,sll2):
-    node = Node()
+    if not sll1.head or not sll2.head:
+        return None
+    
+    if sll1.head == sll2.head:
+        return sll1.head
+    
+    curr1 = sll1.head
+    while curr1.next:
+        curr2 = sll2.head
+        while curr2.next:
+            
+            if curr1.next == curr2.next:
+                return curr1.next
+            
+            curr2 = curr2.next
+        curr1 = curr1.next
 
-    return node
+    return None
 
 #-------------------------------------------------------------
 # Time:
 # Space:
 #-------------------------------------------------------------
+sll1 = LinkedList()
+sll2 = LinkedList()
+sll3 = LinkedList()
+sll4 = LinkedList()
+sll1.group_add([1,2,3])
+sll2.group_add([7,8,9])
+sll3.group_add([4,5,6])
+sll4.group_add([10,11,12,4,5,6])
+
+curr = sll1.head
+curr2 = sll2.head
+curr3 = sll4.head
+
+while curr.next:
+    curr = curr.next
+curr.next = sll3.head
+
+while curr2.next:
+    curr2 = curr2.next
+curr2.next = sll3.head
+
+while curr3.next.value != 4:
+    curr3 = curr3.next
+
+print(is_intersection(sll1,sll4))
+print(is_intersection(sll1,sll2))
 # class test(unittest.TestCase):
+
 
 # if __name__ == '__main__':
 #     unittest.main()
