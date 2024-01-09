e5d4e75d99d01996c831212f93886d99827f392d
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-09 00:55:44 -0500

    Implement Class and push/pop methods in Stack Min.

--- CTCI/Ch3/2_Stack_Min.py
+++ CTCI/Ch3/2_Stack_Min.py
@@ -3,7 +3,43 @@
 # operate in O(1) time.
 
 import unittest
+
 #-------------------------------------------------------------
+class Stack_Node:
+    def __init__(self, value, next_node = None):
+        self.value = value
+        self.next = next_node
+
+    def __str__(self):
+        return str(self.value)
+    
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, value):
+
+        new_node = Stack_Node(value)
+        new_node.next = self.top
+
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            return None
+        
+        old_value = self.top.value
+        self.top = self.top.next
+        
+        return old_value
+    
+    def min(self):
+        if self.top is None:
+            return None
+
+        return
+
+    
 
 #-------------------------------------------------------------
 # Time:
