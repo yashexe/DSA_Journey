41adf5a447f000551497b3be7144602ae500a6d2
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-09 17:38:37 -0500

    Implement Set of Stacks Problem.

--- CTCI/Ch3/3_Stack_Of_Plates.py
+++ CTCI/Ch3/3_Stack_Of_Plates.py
@@ -12,26 +12,123 @@ class SetOfStacks_Node:
         self.value = value
         self.next = next
 
-class SetOfStacks:
+class SetOfStacks_Stack:
     def __init__(self):
+        self.top = None
+        self.num = 0
 
-        self.set = []
+    def __str__(self):
+        curr = self.top
+        result = []
         
+        while curr:
+            result.append(str(curr.value))
+            curr = curr.next
+        
+        return '->'.join(result)
+    
+    def push(self,value):
+        new_node = SetOfStacks_Node(value)
+
+        new_node.next = self.top
+        self.top = new_node
+
+        self.num += 1
+
+    def pop(self):
+        if self.top is None:
+            return None
+        
+        old_node = self.top.value
+
+        self.top = self.top.next
+
+        self.num -= 1
+        return old_node
+
+class SetOfStacks:
+    def __init__(self,threshold = 10):
+        self.threshold = threshold
+        self.set = []
+        self.latest = None
+
+    def __str__(self):
+        if self.is_empty():
+            print('No Sets')
+        for i in range(len(self.set)):
+            print(f'Stack {i}:', self.set[i].__str__())
+            
+    def push(self, value):
+        if self.is_empty():
+            new_stack = SetOfStacks_Stack()
+            new_stack.push(value)
+
+            self.latest = 0
+            self.set.append(None)
+
+            self.set[self.latest] = new_stack
+
+        elif not self.is_empty() and self.set[self.latest].num == self.threshold:
+            new_stack = SetOfStacks_Stack()
+            new_stack.push(value)
+
+            self.latest += 1
+            self.set.append(None)
+
+            self.set[self.latest] = new_stack
+
+        else:
+            self.set[self.latest].push(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        
+        elif not self.is_empty() and self.latest == 0 and self.set[self.latest].num == 1:
+
+            old_value = self.set[self.latest].pop()
+            
+            self.set = []
+            self.latest = None
+
+            return old_value
+        
+        elif not self.is_empty() and self.latest > 0 and self.set[self.latest].num == 1:
+
+            old_value = self.set[self.latest].pop()
+
+            self.set.pop()
+            self.latest -= 1
+
+            return old_value
+        
+        return self.set[self.latest].pop()
+    
+    def is_empty(self):
+        return not self.set
 #-------------------------------------------------------------
-# Time: O(1) - for all operations
-# Space: O(n) - n elements in stack
+# Time: 
+# Space: 
 #-------------------------------------------------------------
 # FOLLOW UP: Implement a function popAt(index) which performs a pop operation on a specific sub-stack
 #-------------------------------------------------------------
-
-
     def popAt(self,index):
+        pass
 
-        return None
+SS = SetOfStacks(3)
+
+SS.push(1)
+SS.push(2)
+SS.push(3)
+SS.push(2)
+SS.push(2)
+
+SS.pop()
+SS.__str__()
 
 #-------------------------------------------------------------
-# Time: O()
-# Space: O()
+# Time: 
+# Space: 
 #-------------------------------------------------------------
 
 # class Test(unittest.TestCase):
