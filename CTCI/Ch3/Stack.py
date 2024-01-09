36b52419ed1f80073f36a223467b9e20b64ed800
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-08 19:28:12 -0500

    Implement Stack_Node and Stack Classes, using Linked List method.

--- CTCI/Ch3/Stack.py
+++ CTCI/Ch3/Stack.py
@@ -1,5 +1,40 @@
 # Stack
 # LIFO: Last in, First out
 
+# 5(self.top) -> 3 -> 1 -> None
 
+class Stack_Node:
+    def __init__(self, value, next_node = None):
+        self.value = value
+        self.next = next_node
+
+    def __str__(self):
+        return str(self.value)
+    
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, value):
+        new_node = Stack_Node(value)
+        new_node.next = self.top
+
+        self.top = new_node
+
+    def pop(self):
+        if self.is_empty():
+            return None
         
+        old_value = self.top.value
+        self.top = self.top.next
+
+        return old_value
+    
+    def peek(self):
+        if self.is_empty():
+            return None
+        
+        return self.top.value
+    
+    def is_empty(self):
+        return self.top is None
