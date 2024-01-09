a8b867980bbfe7708c93dd98ebd245105e0a7ad9
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-09 00:23:39 -0500

    Add Stack Min problem.

---
+++ CTCI/Ch3/2_Stack_Min.py
@@ -0,0 +1,16 @@
+# Stack Min: How would you design a stack which, in addition to push and pop, has a
+# function min which returns the minimum element? Push, pop and min should all
+# operate in O(1) time.
+
+import unittest
+#-------------------------------------------------------------
+
+#-------------------------------------------------------------
+# Time:
+# Space:
+#-------------------------------------------------------------
+
+# class Test(unittest.TestCase):
+
+# if __name__ == '__main__':
+#     unittest.main()
