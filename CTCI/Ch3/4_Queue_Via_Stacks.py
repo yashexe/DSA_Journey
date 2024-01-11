62fe0f285e6dc8e138008410134f515a3760a9e9
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-10 23:57:04 -0500

    Implement MyQueue using one stack.

--- CTCI/Ch3/4_Queue_Via_Stacks.py
+++ CTCI/Ch3/4_Queue_Via_Stacks.py
@@ -11,6 +11,14 @@ class Stack:
     def __init__(self):
         self.top = None
 
+    def __str__(self):
+        curr = self.top
+        stack_list = []
+        while curr:
+            stack_list.append(str(curr.value))
+            curr = curr.next
+        return '->'.join(stack_list)
+    
     def push(self,value):
         new_value = Node(value)
 
@@ -29,9 +37,63 @@ class Stack:
     def peak(self):
         return self.top.value
     
+    def is_empty(self):
+        return self.top is None
+    
 class MyQueue:
     def __init__(self):
-        pass
+        self.queue = Stack()
+
+    def __str__(self):
+        return self.queue.__str__()
+    
+    def enqueue(self, value):
+        self.queue.push(value)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+
+        curr = self.queue.top
+
+        while curr.next.next:
+            curr = curr.next
+        
+        old_value = curr.next.value
+
+        curr.next = None
+        
+        return old_value
+
+    def peak(self):
+        if self.is_empty():
+            return None
+
+        curr = self.queue.top
+
+        while curr.next:
+            curr = curr.next
+        
+        return curr.value
+
+    def is_empty(self):
+        return self.queue.top is None
+Q = MyQueue()
+Q.enqueue(1)
+Q.enqueue(2)
+Q.enqueue(3)
+
+print(Q.__str__())
+print(Q.peak())
+
+Q.dequeue()
+
+print(Q.__str__())
+print(Q.peak())
+
+Q.enqueue(3)
+
+print(Q.__str__())
 #-------------------------------------------------------------
 # Time:
 # Space:
