56ed43cc8ad4e753ddb97f8a6a1941bd7f2d9f66
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-10 22:33:03 -0500

    Implement Node and Stack for Queue via Stacks.

--- CTCI/Ch3/4_Queue_Via_Stacks.py
+++ CTCI/Ch3/4_Queue_Via_Stacks.py
@@ -2,6 +2,33 @@
 
 import unittest
 #-------------------------------------------------------------
+class Node:
+    def __init__(self,value,next=None):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self,value):
+        new_value = Node(value)
+
+        new_value.next = self.top
+        self.top = new_value
+
+    def pop(self):
+        if self.top is None:
+            return None
+        
+        old_value = self.top.value
+        self.top = self.top.next
+
+        return old_value
+    
+    def peak(self):
+        return self.top.value
+    
 class MyQueue:
     def __init__(self):
         pass
