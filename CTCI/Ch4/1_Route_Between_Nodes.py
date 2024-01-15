f75adccb854d3afe4370015528b4e557dd956307
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-14 23:38:11 -0500

    Add Chapter 4 Folder, and Route Between Nodes Problem.

---
+++ CTCI/Ch4/1_Route_Between_Nodes.py
@@ -0,0 +1,17 @@
+# Route Between Nodes: Given a directed graph, design an algorithm to find out
+# whether there is a route between two nodes.
+
+import unittest
+#-------------------------------------------------------------
+
+def checkRoute(g1):
+    pass
+#-------------------------------------------------------------
+# Time: 
+# Space: 
+#-------------------------------------------------------------
+
+class Test(unittest.TestCase):
+
+if __name__ == "__main__":
+    unittest.main()
