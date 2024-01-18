56ce168e31123b6f6b44a8cd4e0924a98aab4571
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-17 23:48:52 -0500

    Add Java BST constructor and insertion method.

--- Data_Structures_Java/BST/BST.java
+++ Data_Structures_Java/BST/BST.java
@@ -1,5 +1,30 @@
 package Data_Structures_Java.BST;
 
 public class BST {
-    
+    private Node root;
+
+    public BST(Node root){
+        root = null;
+    }
+
+    public void insert(int n) {
+        if(root != null) {
+            Node curr = root;
+            insertRecursive(curr, n);
+        }
+        
+        else root = new Node(n, null, null);
+    }
+    private void insertRecursive(Node curr, int n) {
+        if(n > curr.data) {
+            if (curr.right != null) insertRecursive(curr.right, n);
+
+            else curr.right = new Node(n, null, null);
+        }
+        else if (n < curr.data) {
+            if (curr.left != null) insertRecursive(curr.left, n);
+
+            else curr.left = new Node(n, null, null);
+        }
+    }
 }
