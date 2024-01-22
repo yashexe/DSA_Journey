4c04b85f0c81379a9a8f7bfd932620185d9448cf
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-21 23:01:08 -0500

    Add singly linked list implementation for Java.

---
+++ Data_Structures_Java/LinkedList/LinkedList.java
@@ -0,0 +1,80 @@
+package Data_Structures_Java.LinkedList;
+//SLL
+// While iterative traversal is more efficient due to recursion requiring a large call stack
+// for bigger SLLs, both are used in the following class, in efforts to demonstrate knowledge of
+// both.
+
+public class LinkedList {
+    Node head;
+
+    LinkedList() {
+        this.head = null;
+    }
+
+    public void add(int data) {
+        if (isEmpty()) head = new Node(data, null);
+        else add(head.next,data);
+    }
+    private void add(Node curr, int data) {
+        if (curr.next == null) curr.next = new Node(data, null);
+        else add(curr.next, data);
+    }
+    public void groupAdd(int[] arr) {
+        for (int n: arr) add(n);
+    }
+
+    public boolean delete(int data) {
+        if (head.data == data) {
+            head = head.next;
+            return true;
+        }
+        return delete(head.next, data);
+    }
+    private boolean delete(Node curr, int data) {
+        if(curr == null || curr.next == null) return false;
+
+        else if (curr.next.data == data) {
+            curr.next = curr.next.next;
+            return true;
+        }
+        
+        return delete(curr.next, data);
+    }
+
+    public int[] toList() {
+        if (isEmpty()) return new int[0];
+
+        int size = getSize();
+        int[] arr = new int[size];
+        Node curr = head;
+
+        for(int i = 0; i < size; i++) {
+            arr[i] = curr.data;
+            curr = curr.next;
+        }
+        return arr;
+    }
+
+    public int getSize() {
+        if (isEmpty()) return 0;
+        int size = 1;
+        Node curr = head;
+
+        while (curr != null) {
+            size++;
+            curr = curr.next;
+        }
+
+        return size;
+    }
+
+    public void print() {
+        int[] ll = toList();
+
+        for (int n: ll) System.out.print(n + "->");
+    }
+    public boolean isEmpty() {
+        if (head == null) return true;
+        return false;
+    }
+}
---
+++ Data_Structures_Java/LinkedList/Node.java
@@ -0,0 +1,11 @@
+package Data_Structures_Java.LinkedList;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int data, Node next) {
+    this.data = data;
+    this.next = next;
+    }
+}
