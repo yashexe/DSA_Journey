9372092fe040ac5a937c86bd84631ac74ae4a72c
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-23 23:39:41 -0500

    Add Node class, arguments.

--- CTCI/Ch4/1_Route_Between_Nodes.py
+++ CTCI/Ch4/1_Route_Between_Nodes.py
@@ -3,8 +3,12 @@
 
 import unittest
 #-------------------------------------------------------------
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
 
-def checkRoute(g1):
+def checkRoute(g1,n1,n2):
     pass
 #-------------------------------------------------------------
 # Time: 
