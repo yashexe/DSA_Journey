96cde115001dda7e94e9bfd754e96bc5cb3dc09f
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-24 22:22:45 -0500

    Implement checkRoute.

--- CTCI/Ch4/1_Route_Between_Nodes.py
+++ CTCI/Ch4/1_Route_Between_Nodes.py
@@ -4,12 +4,46 @@
 import unittest
 #-------------------------------------------------------------
 class Node:
-    def __init__(self, data, next = None):
-        self.data = data
-        self.next = next
+    def __init__(self, name, ):
+        self.name = name
+        self.children = []
 
-def checkRoute(g1,n1,n2):
-    pass
+    def add_children(self,child):
+        self.children.append(child)
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self,name):
+        self.nodes.append(Node(name))
+
+def checkRoute(n1,n2, nodeDict = {}):
+
+    if n2 in n1.children:
+        return True
+    
+    elif n1 in nodeDict:
+
+        return False
+    
+    for child in n1.children:
+        nodeDict[child] = 1
+        presence = checkRoute(child,n2,nodeDict)
+
+        if presence is True:
+            break
+
+    return presence
+g1 = Graph()
+n1 = Node('Yash')
+n2 = Node('Yash2')
+n3 = Node('Yash3')
+n0 = Node('No')
+n1.add_children(n2)
+n2.add_children(n3)
+
+print(checkRoute(n1,n0))
 #-------------------------------------------------------------
 # Time: 
 # Space: 
