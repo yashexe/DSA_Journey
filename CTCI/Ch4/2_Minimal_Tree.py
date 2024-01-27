5c3884f0c85df718538d4763ad04cb0f586ea12b
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-26 23:57:12 -0500

    Add travserse method.

--- CTCI/Ch4/2_Minimal_Tree.py
+++ CTCI/Ch4/2_Minimal_Tree.py
@@ -2,11 +2,31 @@
 # write an algorithm to create a binary search tree with minimal height
 
 import unittest
-from Binary_Search_Tree import Tree_Node, insert
+from Binary_Search_Tree import Tree_Node
 #-------------------------------------------------------------
 
 def minimalTree(arr):
+    insert(arr)
 
+def insert(arr, root = None):
+    if root is None:
+        root = Tree_Node(arr.pop(getMidpoint(arr)))
+
+    elif arr == []:
+        return
+    elif root and arr:
+        pass
+    
+def getMidpoint(arr):
+    return (len(arr) - 1) // 2
+
+def travserse(data, root):
+    if not root.left and not root.right:
+        return root
+    if root.left and root.left > data:
+        root = travserse(data, root.left)
+    if root.left and root.right < data:
+        root = travserse(data, root.right)
 #-------------------------------------------------------------
 # Time: 
 # Space: 
