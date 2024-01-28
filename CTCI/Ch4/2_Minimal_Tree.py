fced0b687a7d9db33c111642922366ea7a92fc5f
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-28 00:54:24 -0500

    Complete Minimal Tree

--- CTCI/Ch4/2_Minimal_Tree.py
+++ CTCI/Ch4/2_Minimal_Tree.py
@@ -2,19 +2,38 @@
 # write an algorithm to create a binary search tree with minimal height
 
 import unittest
-from Binary_Search_Tree import Tree_Node
+from Binary_Search_Tree import Tree_Node, insert,in_order_traverse, pre_order_traverse, post_order_traverse
 #-------------------------------------------------------------
 
 def minimalTree(arr):
     if not arr:
         return
-    root = Tree_Node(arr.pop((len(arr)) - 1) // 2)
     
-    minimalTreeRecursive(arr,root)
+    mid = getMidpoint(arr)
+    root = Tree_Node(arr.pop(mid))
 
-def minimalTreeRecursive(arr, node = None):
+    minimalTreeRecursive(arr[:mid],root)
+    minimalTreeRecursive(arr[mid:],root)
+
+    return root
+
+def minimalTreeRecursive(arr, root = None):
     if not arr:
         return
+
+    mid = getMidpoint(arr)
+
+    insert(root,arr[mid])
+
+    if mid == 0:
+        return
+    
+    minimalTreeRecursive(arr[mid:], root)
+    minimalTreeRecursive(arr[:mid], root)
+
+def getMidpoint(arr):
+    return len(arr) // 2
+
 #-------------------------------------------------------------
 # Time: 
 # Space: 
