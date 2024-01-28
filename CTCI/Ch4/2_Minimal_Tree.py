5d659132552eb7f88df66b6668bf55b7545ec57c
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-28 15:51:01 -0500

    Add time/space and test cases.

--- CTCI/Ch4/2_Minimal_Tree.py
+++ CTCI/Ch4/2_Minimal_Tree.py
@@ -2,7 +2,7 @@
 # write an algorithm to create a binary search tree with minimal height
 
 import unittest
-from Binary_Search_Tree import Tree_Node, insert,in_order_traverse, pre_order_traverse, post_order_traverse
+from Binary_Search_Tree import Tree_Node, insert, get_height
 #-------------------------------------------------------------
 
 def minimalTree(arr):
@@ -11,7 +11,7 @@ def minimalTree(arr):
     
     mid = getMidpoint(arr)
     root = Tree_Node(arr.pop(mid))
-
+    mid = getMidpoint(arr)
     minimalTreeRecursive(arr[:mid],root)
     minimalTreeRecursive(arr[mid:],root)
 
@@ -35,12 +35,29 @@ def getMidpoint(arr):
     return len(arr) // 2
 
 #-------------------------------------------------------------
-# Time: 
-# Space: 
+# Time: O(nlogn) - Array is split in half into 1 element segments
+# Space: log(n) - balanced tree, O(1) for "mid"
 #-------------------------------------------------------------
 
-# class Test(unittest.TestCase):
-#     def setUp(self):
+class Test(unittest.TestCase):
+    def test_minimalTree_empty(self):
+        arr_empty = []
+        self.assertIsNone(minimalTree(arr_empty))
 
-# if __name__ == "__main__":
-#     unittest.main()
+    def test_minimalTree_single(self):
+        arr_single = [5]
+        root_single = minimalTree(arr_single)
+        self.assertEqual(get_height(root_single), 1)
+
+    def test_minimalTree_odd(self):
+        arr_odd = [1, 2, 3, 4, 5]
+        root_odd = minimalTree(arr_odd)
+        self.assertTrue(0 <= abs(get_height(root_odd.left) - get_height(root_odd.right)) <= 1)
+
+    def test_minimalTree_even(self):
+        arr_even = [1, 2, 3, 4]
+        root_even = minimalTree(arr_even)
+        self.assertTrue(0 <= abs(get_height(root_even.left) - get_height(root_even.right)) <= 1)
+
+if __name__ == "__main__":
+    unittest.main()
