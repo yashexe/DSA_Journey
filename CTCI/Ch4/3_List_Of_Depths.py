066cce5d6b62ebc41c18279be550a3d489985bfd
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-01-29 23:22:19 -0500

    Add List of Depths problem.

---
+++ CTCI/Ch4/3_List_Of_Depths.py
@@ -0,0 +1,21 @@
+# List of Depths: Given a binary tree, design an algorithm which creates a linked
+# list of all the nodes at each depth.
+
+# e.g., if you have a tree with depth D, you'll have D linked lists
+
+import unittest
+from Binary_Search_Tree import Tree_Node
+#-------------------------------------------------------------
+
+def listDepths(root):
+    pass
+
+#-------------------------------------------------------------
+# Time: 
+# Space: 
+#-------------------------------------------------------------
+
+# class Test(unittest.TestCase):
+
+# if __name__ == "__main__":
+#     unittest.main()
