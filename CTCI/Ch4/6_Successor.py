66c47a611f476ff05f942a1d5a97b00a91ea6ffa
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-02-07 22:39:32 -0500

    Add Successor Problem.

---
+++ CTCI/Ch4/6_Successor.py
@@ -0,0 +1,16 @@
+# Successor: Write an algorithm to find the "next" node(i.e., in-order successor) of a given node in a BST.
+# You may assume each node has a link to it's parent.
+import unittest
+from Binary_Search_Tree import Tree_Node,insert
+#-------------------------------------------------------------
+def findSuccessor(node):
+    pass
+#-------------------------------------------------------------
+# Time: 
+# Space: 
+#-------------------------------------------------------------
+# class Test(unittest.TestCase):
+#     def setUp(self):
+        
+# if __name__ == "__main__":
+#     unittest.main()
