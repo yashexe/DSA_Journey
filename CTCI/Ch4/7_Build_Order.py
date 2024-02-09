3cf7ff59412ab229eb3b93787d0c1b83b966bb5b
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-02-08 23:16:50 -0500

    Add Build Order Problem.

---
+++ CTCI/Ch4/7_Build_Order.py
@@ -0,0 +1,22 @@
+# Build Order: You are given a list of projects and a list of dependancies (which is
+# a list of pairs of projects, where the 2nd project is dependant on the first project). All
+# of a project's dependancies must be built before the project is. Find a build order
+# that will allow the projects to be built. If there is no valid order, return an error.
+# EX:
+#   Input: projects: [a, b, c, d, e, f] dependancies: [(a,d), (f,b), (b,d), (f,a), (d,c)]
+#   Output: [f, e, a, b, d, c]
+
+import unittest
+from Binary_Search_Tree import Tree_Node,insert
+#-------------------------------------------------------------
+def buildOrder(projects, dependancies):
+    pass
+#-------------------------------------------------------------
+# Time:
+# Space:
+#-------------------------------------------------------------
+# class Test(unittest.TestCase):
+#     def setUp(self):
+        
+# if __name__ == "__main__":
+#     unittest.main()
