2fd9dbc5e4f0283bb5e4c9bdb9a66b095611a7bf
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-02-14 23:41:05 -0500

    BST Sequences.

---
+++ CTCI/Ch4/9_BST_Sequences.py
@@ -0,0 +1,19 @@
+# BST Sequences: A BST was created by traversing through an array from left to right
+# and inserting each element. Given a BST with distinct elements, print all possible
+# arrays that couldve lead to this tree.
+# EXAMPLE: 2
+#         1 3
+# Output: {2, 1, 3}, {2, 3, 1}
+import unittest
+from Binary_Search_Tree import Tree_Node
+#-------------------------------------------------------------
+def BST_Sequences(root):
+    pass
+#-------------------------------------------------------------
+# Time: 
+# Space: 
+#-------------------------------------------------------------
+# class Test(unittest.TestCase):
+#     pass
+# if __name__ == "__main__":
+#     unittest.main()
