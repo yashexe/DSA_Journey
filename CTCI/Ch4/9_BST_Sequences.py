00627a9cc19f060f37ce36538923d70f852a2367
Yash Bhavsar <bhavsary@mcmaster.ca>
2024-02-16 23:58:22 -0500

    Update BST Sequences.

--- CTCI/Ch4/9_BST_Sequences.py
+++ CTCI/Ch4/9_BST_Sequences.py
@@ -12,7 +12,15 @@ def BST_Sequences(root):
 
     arr = toArr(root, [0 for i in range(arrSize)])
 
-    return arr
+    return checkSequences(arr, [arr], arrSize)
+
+def getHeight(root, height = 0):
+    if root is None: return height
+
+    l = getHeight(root.left, height)
+    r = getHeight(root.right, height)
+
+    return max(l, r) + 1
 
 def toArr(root, arr, i = 0):
     if root is None: return arr
@@ -24,21 +32,43 @@ def toArr(root, arr, i = 0):
 
     return arr
 
-def getHeight(root, height = 0):
-    if root is None: return height
+def checkSequences(arr, sequences, size, i = 0):
+    if i > size - 1 or arr[i] == 0:
+        return
     
-    l = getHeight(root.left, height)
-    r = getHeight(root.left, height)
+    left = checkChildren(arr, 2*i+1, size)
+    right = checkChildren(arr, 2*i+2, size)
 
-    return max(l,r) + 1
+    if left:
+        sequences.append(left)
+    if right:
+        sequences.append(right)
+
+    checkSequences(arr, sequences, size, 2*i+1)
+    checkSequences(arr, sequences, size, 2*i+2)
+
+    return sequences
+
+def checkChildren(arr, i, size):
+    if i >= (size - 1)/2:
+        return None
+    
+    if arr[2*i + 1] != 0 and arr[2*i + 2] != 0:
+        tempArr = arr
+        tempArr[2*i + 1], tempArr[2*i + 2] = tempArr[2*i + 2], tempArr[2*i + 1]
+        return tempArr
+    else:
+        return None
+
 #-------------------------------------------------------------
 # Time: 
 # Space: 
 #-------------------------------------------------------------
 BST = Tree_Node(5)
 insert_multiple(BST, [3, 7, 2, 4, 6, 8])
+sequences = BST_Sequences(BST)
 
-print(BST_Sequences(BST))
+for sequence in sequences: print(sequence)
 # class Test(unittest.TestCase):
 #     pass
 # if __name__ == "__main__":
